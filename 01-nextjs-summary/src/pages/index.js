8c1b0fa71949759492af537016abd3887c49fb22
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-02 09:39:18 +0530

    Meeetup Detail page created

--- 01-nextjs-summary/src/pages/index.js
+++ 01-nextjs-summary/src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import MeetupList from '@/components/meetups/MeetupList'
 
 const inter = Inter({ subsets: ['latin'] })
