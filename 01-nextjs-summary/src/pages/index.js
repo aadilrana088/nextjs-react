9f2bf7c0b49529ee63c5ea31d4036dd62e67ad46
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-03 11:18:01 +0530

    Fetched Data from Mongodb and displayed it

--- 01-nextjs-summary/src/pages/index.js
+++ 01-nextjs-summary/src/pages/index.js
@@ -1,25 +1,26 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import MeetupList from '@/components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup!',
-    },
-    {
-        id: 'm2',
-        title: 'A Second Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 10, 12345 Some City',
-        description: 'This is a second meetup!',
-    },
-];
+// const DUMMY_MEETUPS = [
+//     {
+//         id: 'm1',
+//         title: 'A First Meetup',
+//         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Some address 5, 12345 Some City',
+//         description: 'This is a first meetup!',
+//     },
+//     {
+//         id: 'm2',
+//         title: 'A Second Meetup',
+//         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Some address 10, 12345 Some City',
+//         description: 'This is a second meetup!',
+//     },
+// ];
 
 export default function Home(props) {
     return (
@@ -45,11 +46,20 @@ export default function Home(props) {
 
 export async function getServerSideProps() {
     // Fetch data from external API
+    const client = await MongoClient.connect(process.env.MONGO_URL);
+    const db = client.db();
 
+    const meetupsCollection = db.collection('meetups');
+    const meetups = await meetupsCollection.find().toArray();
     // Pass data to the page via props
     return {
         props: {
-            meetups: DUMMY_MEETUPS,
+            meetups: meetups.map((meetup) => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString(),
+            })),
         },
     };
 }
