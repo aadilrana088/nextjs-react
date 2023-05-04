d96f21647a1935458149eb7c107d6177c5b5a929
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-04 19:35:07 +0530

    Created UI for event search and read value of filter

--- 02-getting-started/components/ui/button.js
+++ 02-getting-started/components/ui/button.js
@@ -3,10 +3,17 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 function Button(props) {
+    if (props.link) {
+        return (
+            <Link href={props.link} className={classes.btn}>
+                {props.children}
+            </Link>
+        );
+    }
     return (
-        <Link href={props.link}>
-            <span className={classes.btn}>{props.children}</span>
-        </Link>
+        <button className={classes.btn} onClick={props.onClick}>
+            {props.children}
+        </button>
     );
 }
 
