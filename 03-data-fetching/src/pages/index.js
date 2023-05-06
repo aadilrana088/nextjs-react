a8bc1ab3d5f0d432028f97f34a8e311dcc596e8e
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-06 12:10:35 +0530

    Fetch data using getstaticProps and revalidate it

--- 03-data-fetching/src/pages/index.js
+++ 03-data-fetching/src/pages/index.js
@@ -1,20 +1,42 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+import path from 'path';
+import fs from 'fs/promises';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Data fetching in Nextjs</title>
-        <meta name="description" content="Data fetching in Nextjs" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${inter.className}`}>
-        Nextjs Starting Project
-      </main>
-    </>
-  )
+export default function Home(props) {
+    const { products } = props;
+    return (
+        <>
+            <Head>
+                <title>Data fetching in Nextjs</title>
+                <meta name="description" content="Data fetching in Nextjs" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={`${inter.className}`}>
+                {products.map((product) => (
+                    <li key={product.id}>{product.title}</li>
+                ))}
+            </main>
+        </>
+    );
+}
+
+export async function getStaticProps() {
+    console.log('(Re-)Generating...');
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    return {
+        props: {
+            products: data.products,
+        },
+        revalidate: 10,
+    };
 }
