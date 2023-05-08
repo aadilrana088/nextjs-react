fe55d81d990d0264c2e15414a416ccb6b1282e73
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-08 14:09:34 +0530

    Use SWR for data fetching

--- 03-data-fetching/src/pages/last-sales.js
+++ 03-data-fetching/src/pages/last-sales.js
@@ -1,45 +1,64 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 function LastSalesPage() {
     const [sales, setSales] = useState();
-    const [isLoading, setIsLoading] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+    const { data, error } = useSWR(
+        'https://nextjs-course-9c14d-default-rtdb.firebaseio.com/sales.json', fetcher
+    );
 
     useEffect(() => {
-        setIsLoading(true);
-        fetch(
-            'https://nextjs-course-9c14d-default-rtdb.firebaseio.com/sales.json'
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                const transformedSales = [];
+        if (data) {
+            const transformedSales = [];
 
-                for (const key in data) {
-                    transformedSales.push({
-                        id: key,
-                        username: data[key].username,
-                        volume: data[key].volume,
-                    });
-                }
+            for (const key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume,
+                });
+            }
 
-                setSales(transformedSales);
-                setIsLoading(false);
-            });
-    }, []);
+            setSales(transformedSales);
+        }
+    }, [data]);
 
-    if (isLoading) {
-        return <p>Loading....</p>;
+    // useEffect(() => {
+    //   setIsLoading(true);
+    //   fetch('https://nextjs-course-9c14d-default-rtdb.firebaseio.com/sales.json')
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       const transformedSales = [];
+
+    //       for (const key in data) {
+    //         transformedSales.push({
+    //           id: key,
+    //           username: data[key].username,
+    //           volume: data[key].volume,
+    //         });
+    //       }
+
+    //       setSales(transformedSales);
+    //       setIsLoading(false);
+    //     });
+    // }, []);
+
+    if (error) {
+        return <p>Failed to load.</p>;
     }
 
-    if (!sales) {
-        return <p>No data yet</p>;
+    if (!data || !sales) {
+        return <p>Loading...</p>;
     }
 
     return (
         <ul>
             {sales.map((sale) => (
                 <li key={sale.id}>
-                    {sale.username} - {sale.volume}
+                    {sale.username} - ${sale.volume}
                 </li>
             ))}
         </ul>
