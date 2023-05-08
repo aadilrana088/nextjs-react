bb26f8cb926f1969f2a68da46c102cc550ddda60
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-08 11:27:49 +0530

    Get seerversideprops and print page uid

--- 03-data-fetching/src/pages/index.js
+++ 03-data-fetching/src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home(props) {
             <main className={`${inter.className}`}>
                 {products.map((product) => (
                     <li key={product.id}>
-                        <Link href={product.id}>{product.title}</Link>
+                        <Link href={`/products/${product.id}`}>{product.title}</Link>
                     </li>
                 ))}
             </main>
