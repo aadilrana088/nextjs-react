05d311ccd428310cc6e0fbc5397542856b5e5142
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-09 12:06:16 +0530

    Fetch data of dynamic page and use getstaticpath

--- 02-getting-started/src/pages/events/[eventId].js
+++ 02-getting-started/src/pages/events/[eventId].js
@@ -2,21 +2,28 @@ import EventContent from '@/components/event-detail/event-content';
 import EventLogistics from '@/components/event-detail/event-logistics';
 import EventSummary from '@/components/event-detail/event-summary';
 import ErrorAlert from '@/components/ui/error-alert';
-import { getEventById } from 'dummy-data';
+// import { getAllEvents, getEventById } from 'dummy-data';
+import { getAllEvents, getEventById } from 'helpers/api-util';
 import { useRouter } from 'next/router';
 
-function EventDetailPage() {
-    const router = useRouter();
-    const eventId = router.query.eventId;
-    const event = getEventById(eventId);
+function EventDetailPage(props) {
+    // const router = useRouter();
+    // const eventId = router.query.eventId;
+    // const event = getEventById(eventId);
+
+    const { event } = props;
 
     if (!event) {
-        return (
-            <ErrorAlert>
-                <p>No event found!</p>
-            </ErrorAlert>
-        );
+        return <p className="center">Loading...</p>;
     }
+
+    // if (!event) {
+    //     return (
+    //         <ErrorAlert>
+    //             <p>No event found!</p>
+    //         </ErrorAlert>
+    //     );
+    // }
 
     return (
         <>
@@ -35,3 +42,21 @@ function EventDetailPage() {
 }
 
 export default EventDetailPage;
+
+
+export async function getStaticPaths() {
+    const events = await getAllEvents();
+    const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+    return {
+        paths: paths,
+        fallback: false, // can also be true or 'blocking'
+    };
+}
+
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+    const event = await getEventById(eventId);
+
+    return { props: { event } };
+}
