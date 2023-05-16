c1efedf982919c4bbabebb9919ffd0033ba76a9c
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-16 20:55:39 +0530

    Subscribtion added and added validation

--- 05-project-api-routes/components/input/newsletter-registration.js
+++ 05-project-api-routes/components/input/newsletter-registration.js
@@ -1,30 +1,55 @@
+import { useRef, useState } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+    const emailInputRef = useRef();
+    const [isInvalid, setIsInvalid] = useState(false);
+    function registrationHandler(event) {
+        event.preventDefault();
+        // fetch user input (state or refs)
+        const enteredEmail = emailInputRef.current.value;
+        // optional: validate input
+        if (
+            !enteredEmail ||
+            enteredEmail.trim() === '' ||
+            !enteredEmail.includes('@')
+        ) {
+            setIsInvalid(true);
+        }
+        // send valid data to API
+        fetch('/api/newsletter/', {
+            method: 'POST',
+            body: JSON.stringify({ email: enteredEmail }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-  }
-
-  return (
-    <section className={classes.newsletter}>
-      <h2>Sign up to stay updated!</h2>
-      <form onSubmit={registrationHandler}>
-        <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-          />
-          <button>Register</button>
-        </div>
-      </form>
-    </section>
-  );
+    return (
+        <section className={classes.newsletter}>
+            <h2>Sign up to stay updated!</h2>
+            <form onSubmit={registrationHandler}>
+                <div className={classes.control}>
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="Your email"
+                        aria-label="Your email"
+                        ref={emailInputRef}
+                    />
+                    <button>Register</button>
+                </div>
+                {isInvalid && (
+                    <p style={{ color: 'red' }}>
+                        Please enter proper email address
+                    </p>
+                )}
+            </form>
+        </section>
+    );
 }
 
 export default NewsletterRegistration;
