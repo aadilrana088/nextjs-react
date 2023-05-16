cd94ef9b6dd25c240f2809b47961f3b7cae74186
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-16 22:26:34 +0530

    Connect to Mongodba and save email to db

--- 05-project-api-routes/src/pages/api/newsletter.js
+++ 05-project-api-routes/src/pages/api/newsletter.js
@@ -1,12 +1,19 @@
-function handler(req, res) {
-    if(req.method === 'POST') {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+    if (req.method === 'POST') {
         const userEmail = req.body.email;
-        if(!userEmail || !userEmail.includes('@')) {
-            res.status(422).json({message: 'Invalid Email address'});
-            return
+        if (!userEmail || !userEmail.includes('@')) {
+            res.status(422).json({ message: 'Invalid Email address' });
+            return;
         }
-        console.log(userEmail);
-        res.status(201).json({message:'Signed Up!'})
+
+        const client = await MongoClient.connect(process.env.MONGO_URL);
+        const db = client.db();
+        await db.collection('emails').insertOne({ email: userEmail });
+        client.close();
+        
+        res.status(201).json({ message: 'Signed Up!' });
     }
 }
-export default handler
+export default handler;
