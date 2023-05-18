802d44704208b95d3f1d1494698e96df6693aba2
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-18 19:53:43 +0530

    Notification UI added

--- 05-project-api-routes/components/layout/layout.js
+++ 05-project-api-routes/components/layout/layout.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
 
 function Layout(props) {
     return (
         <Fragment>
             <MainHeader />
             <main>{props.children}</main>
+            <Notification title="success" message="Data Inserted Successfully" status="success" />
         </Fragment>
     );
 }
