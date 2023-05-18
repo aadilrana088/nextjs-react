214eae773debbb3be8605ff03986eff86cb2af30
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-18 20:21:39 +0530

    Show notification on request of newsletter registeration

--- 05-project-api-routes/components/input/newsletter-registration.js
+++ 05-project-api-routes/components/input/newsletter-registration.js
@@ -1,13 +1,23 @@
-import { useRef, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
 import classes from './newsletter-registration.module.css';
+import NotificationContext from 'store/notification-context';
 
 function NewsletterRegistration() {
     const emailInputRef = useRef();
     const [isInvalid, setIsInvalid] = useState(false);
+    const notificationCtx = useContext(NotificationContext);
+
     function registrationHandler(event) {
         event.preventDefault();
         // fetch user input (state or refs)
         const enteredEmail = emailInputRef.current.value;
+
+        notificationCtx.showNotification({
+            title: 'Signing up...',
+            message: 'Registering for newsletter.',
+            status: 'pending',
+        });
+
         // optional: validate input
         if (
             !enteredEmail ||
@@ -24,8 +34,28 @@ function NewsletterRegistration() {
                 'Content-Type': 'application/json',
             },
         })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                return response.json().then((data) => {
+                    throw new Error(data.message || 'Something went wrong');
+                });
+            })
+            .then((data) => {
+                notificationCtx.showNotification({
+                    title: 'Success!',
+                    message: 'Successfully registered for newsletter!',
+                    status: 'success',
+                });
+            })
+            .catch((error) => {
+                notificationCtx.showNotification({
+                    title: 'Error!',
+                    message: error.message || 'Something went wrong!',
+                    status: 'error',
+                });
+            });
     }
 
     return (
