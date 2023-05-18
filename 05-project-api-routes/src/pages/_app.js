58642700bba83667a29e2d1c5dd7deebbe0f0bf4
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-18 20:11:09 +0530

    Create contextprovider for notification

--- 05-project-api-routes/src/pages/_app.js
+++ 05-project-api-routes/src/pages/_app.js
@@ -1,20 +1,23 @@
 import Layout from '@/components/layout/layout';
 import '@/styles/globals.css';
 import Head from 'next/head';
+import { NotificationContextProvider } from 'store/notification-context';
 
 export default function App({ Component, pageProps }) {
     return (
-        <Layout>
-            <Head>
-                <title>NextJS Events</title>
-                <meta
-                    name="description"
-                    content="Great event by nextjs"
-                />
-                <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Component {...pageProps} />
-        </Layout>
+        <NotificationContextProvider>
+            <Layout>
+                <Head>
+                    <title>NextJS Events</title>
+                    <meta name="description" content="Great event by nextjs" />
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1.0, width=device-width"
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <Component {...pageProps} />
+            </Layout>
+        </NotificationContextProvider>
     );
 }
