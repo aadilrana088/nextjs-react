83a3db4d25bbb3da257a79092e8646fa36758848
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-24 23:19:11 +0530

    created featured posts & displays it

--- 06-nextjs-blog/components/layout/logo.js
+++ 06-nextjs-blog/components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from './logo.module.css';
 
 function Logo() {
-    return <div className={classes.logo}>Rana Next Blog</div>;
+    return <div className={classes.logo}><i>Rana</i> Blog</div>;
 }
 
 export default Logo;
