a35138f7e6affb4246f909d25523319a9cf0e3ff
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-27 23:24:48 +0530

    Updated image from markdown file

--- 06-nextjs-blog/components/posts/post-detail/post-content.js
+++ 06-nextjs-blog/components/posts/post-detail/post-content.js
@@ -1,7 +1,7 @@
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
-
+import Image from 'next/image';
 // const DUMMY_POST = {
 //     slug: 'getting-started-with-nextjs',
 //     title: 'Getting Started with NextJS',
@@ -15,10 +15,63 @@ function PostContent(props) {
     // const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+    const customRenderers = {
+        // image(image) {
+        //   return (
+        //     <Image
+        //       src={`/images/posts/${post.slug}/${image.src}`}
+        //       alt={image.alt}
+        //       width={600}
+        //       height={300}
+        //     />
+        //   );
+        // },
+        p(paragraph) {
+            const { node } = paragraph;
+            console.log('Node: ' + node);
+            if (node.children[0].type === 'image') {
+                const image = node.children[0];
+                console.log(image);
+                return (
+                    <div className={classes.image}>
+                        <Image
+                            src={`/images/posts/${post.slug}/${image.url}`}
+                            alt={image.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                );
+            }
+
+            return <p>{paragraph.children}</p>;
+        },
+    };
+
     return (
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown
+                components={{
+                    img: function ({ ...props }) {
+                        const substrings = props.alt?.split('{{');
+                        const alt = substrings[0].trim();
+
+                        return (
+                            <div className={classes.image}>
+                                <Image
+                                    src={`/images/posts/${post.slug}/${props.src}`}
+                                    alt={alt}
+                                    width={600}
+                                    height={300}
+                                />
+                            </div>
+                        );
+                    },
+                }}
+            >
+                {post.content}
+            </ReactMarkdown>
         </article>
     );
 }
