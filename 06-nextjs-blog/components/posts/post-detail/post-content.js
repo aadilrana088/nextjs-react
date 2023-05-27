9c40dad4bbc989063630eb2f6b68ad43ddb5fd7a
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-05-28 00:32:21 +0530

    Enter second blog and print it

--- 06-nextjs-blog/components/posts/post-detail/post-content.js
+++ 06-nextjs-blog/components/posts/post-detail/post-content.js
@@ -1,18 +1,14 @@
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
-import Image from 'next/image';
-// const DUMMY_POST = {
-//     slug: 'getting-started-with-nextjs',
-//     title: 'Getting Started with NextJS',
-//     image: 'getting-started-nextjs.png',
-//     date: '2022-02-10',
-//     content: '# This is a first post',
-// };
 
 function PostContent(props) {
     const { post } = props;
-    // const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
     const customRenderers = {
@@ -28,9 +24,10 @@ function PostContent(props) {
         // },
         p(paragraph) {
             const { node } = paragraph;
+
             if (node.children[0].tagName === 'img') {
                 const image = node.children[0];
-                console.log(image);
+
                 return (
                     <div className={classes.image}>
                         <Image
@@ -45,14 +42,24 @@ function PostContent(props) {
 
             return <p>{paragraph.children}</p>;
         },
+
+        // code(code) {
+        //     const { className, children } = code;
+        //     const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+        //     return (
+        //         <SyntaxHighlighter
+        //             style={atomDark}
+        //             language={language}
+        //             children={children}
+        //         />
+        //     );
+        // },
     };
 
     return (
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown
-                components={customRenderers}
-            >
+            <ReactMarkdown components={customRenderers}>
                 {post.content}
             </ReactMarkdown>
         </article>
