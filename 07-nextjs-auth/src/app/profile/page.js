c61b5b80768c08fbfcf6db76a5340e3c20626bf3
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-06-03 00:33:33 +0530

    route protection with serverside

--- 07-nextjs-auth/src/app/profile/page.js
+++ 07-nextjs-auth/src/app/profile/page.js
@@ -1,6 +1,15 @@
+import { getSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 import UserProfile from '../../../components/profile/user-profile';
 
-function ProfilePage() {
+async function ProfilePage({ params }) {
+    const session = await getSession({ req: params.req });
+    if (!session) {
+        console.log("true");
+        if (!session) {
+            redirect('/auth')
+        }
+    }
     return <UserProfile />;
 }
 
